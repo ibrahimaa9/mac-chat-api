68767856b4d5d06d2658618a99153946459bcda3
ibrahim <ibrahimaa9@gmail.com>
2019-02-18 21:59:29 +0300

    add Sualf-Chat url

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
+  "mongoUrl": "mongodb://ibrahim:pro97@sualfdb-shard-00-00-vmvjj.mongodb.net:27017,sualfdb-shard-00-01-vmvjj.mongodb.net:27017,sualfdb-shard-00-02-vmvjj.mongodb.net:27017/admin?replicaSet=SualfDB-shard-0&ssl=true"
   "bodyLimit": "100kb"
 }
